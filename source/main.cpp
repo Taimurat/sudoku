9705bcb708415b6ebfa767f18c45ac33711234b9
Taimurat <timfrolserg@mail.ru>
2024-10-15 22:14:42 +0500

    reference to testSudoku

--- source/main.cpp
+++ source/main.cpp
@@ -9,59 +9,87 @@
 //#define DEBUG
 
 int main() {
+
+    Sudoku voidSudoku(vector<vector<int>>
+    {
+        {0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0}
+    });
     Sudoku easiestSudoku(vector<vector<int>>
     {
-        vector<int> {3, 0, 0, 9, 6, 7, 0, 0, 1},
-        vector<int> {0, 4, 0, 3, 0, 2, 0, 8, 0},
-        vector<int> {0, 2, 0, 0, 0, 0, 0, 7, 0},
-        vector<int> {0, 7, 0, 0, 0, 0, 0, 9, 0},
-        vector<int> {0, 0, 0, 8, 7, 3, 0, 0, 0},
-        vector<int> {5, 0, 0, 0, 1, 0, 0, 0, 3},
-        vector<int> {0, 0, 4, 7, 0, 5, 1, 0, 0},
-        vector<int> {9, 0, 5, 0, 0, 0, 2, 0, 7},
-        vector<int> {8, 0, 0, 6, 2, 1, 0, 0, 4}
+        {3, 0, 0, 9, 6, 7, 0, 0, 1},
+        {0, 4, 0, 3, 0, 2, 0, 8, 0},
+        {0, 2, 0, 0, 0, 0, 0, 7, 0},
+        {0, 7, 0, 0, 0, 0, 0, 9, 0},
+        {0, 0, 0, 8, 7, 3, 0, 0, 0},
+        {5, 0, 0, 0, 1, 0, 0, 0, 3},
+        {0, 0, 4, 7, 0, 5, 1, 0, 0},
+        {9, 0, 5, 0, 0, 0, 2, 0, 7},
+        {8, 0, 0, 6, 2, 1, 0, 0, 4}
     });
     Sudoku gentleSudoku(vector<vector<int>>
     {
-        vector<int> {0, 0, 0, 0, 0, 4, 0, 2, 8},
-        vector<int> {4, 0, 6, 0, 0, 0, 0, 0, 5},
-        vector<int> {1, 0, 0, 0, 3, 0, 6, 0, 0},
-        vector<int> {0, 0, 0, 3, 0, 1, 0, 0, 0},
-        vector<int> {0, 8, 7, 0, 0, 0, 1, 4, 0},
-        vector<int> {0, 0, 0, 7, 0, 9, 0, 0, 0},
-        vector<int> {0, 0, 2, 0, 1, 0, 0, 0, 3},
-        vector<int> {9, 0, 0, 0, 0, 0, 5, 0, 7},
-        vector<int> {6, 7, 0, 4, 0, 0, 0, 0, 0}
+        {0, 0, 0, 0, 0, 4, 0, 2, 8},
+        {4, 0, 6, 0, 0, 0, 0, 0, 5},
+        {1, 0, 0, 0, 3, 0, 6, 0, 0},
+        {0, 0, 0, 3, 0, 1, 0, 0, 0},
+        {0, 8, 7, 0, 0, 0, 1, 4, 0},
+        {0, 0, 0, 7, 0, 9, 0, 0, 0},
+        {0, 0, 2, 0, 1, 0, 0, 0, 3},
+        {9, 0, 0, 0, 0, 0, 5, 0, 7},
+        {6, 7, 0, 4, 0, 0, 0, 0, 0}
     });
 
     Sudoku moderateSudoku(vector<vector<int>>
     {
-        vector<int> {7, 2, 0, 0, 9, 6, 0, 0, 3},
-        vector<int> {0, 0, 0, 2, 0, 5, 0, 0, 0},
-        vector<int> {0, 8, 0, 0, 0, 4, 0, 2, 0},
-        vector<int> {0, 0, 0, 0, 0, 0, 0, 6, 0},
-        vector<int> {1, 0, 6, 5, 0, 3, 8, 0, 7},
-        vector<int> {0, 4, 0, 0, 0, 0, 0, 0, 0},
-        vector<int> {0, 3, 0, 8, 0, 0, 0, 9, 0},
-        vector<int> {0, 0, 0, 7, 0, 2, 0, 0, 0},
-        vector<int> {2, 0, 0, 4, 3, 0, 0, 1, 8}
+        {7, 2, 0, 0, 9, 6, 0, 0, 3},
+        {0, 0, 0, 2, 0, 5, 0, 0, 0},
+        {0, 8, 0, 0, 0, 4, 0, 2, 0},
+        {0, 0, 0, 0, 0, 0, 0, 6, 0},
+        {1, 0, 6, 5, 0, 3, 8, 0, 7},
+        {0, 4, 0, 0, 0, 0, 0, 0, 0},
+        {0, 3, 0, 8, 0, 0, 0, 9, 0},
+        {0, 0, 0, 7, 0, 2, 0, 0, 0},
+        {2, 0, 0, 4, 3, 0, 0, 1, 8}
     });
+    Sudoku nakedMadnessSudoku(vector<vector<int>>
+    {
+        {0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 1, 9, 0, 0, 5, 0, 0},
+        {5, 6, 0, 3, 1, 0, 0, 9, 0},
+        {1, 0, 0, 6, 0, 0, 0, 2, 8},
+        {0, 0, 4, 0, 0, 0, 7, 0, 0},
+        {2, 7, 0, 0, 0, 4, 0, 0, 3},
+        {0, 4, 0, 0, 6, 8, 0, 3, 5},
+        {0, 0, 2, 0, 0, 5, 9, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0}
+    });
+
 
     SudokuSolver::initializeStrategies();
 
+    Sudoku& testSudoku = nakedMadnessSudoku;
+
     #ifdef DEBUG
-    moderateSudoku.debugDisplay();
-    SudokuSolver slv(&moderateSudoku);
-    std::cout << moderateSudoku.GetIsValid() << std::endl;
+    testSudoku.debugDisplay();
+    SudokuSolver slv(&testSudoku);
+    std::cout << testSudoku.GetIsValid() << std::endl;
     slv.solve();
-    moderateSudoku.debugDisplay();
+    testSudoku.debugDisplay();
 
     #else
-    moderateSudoku.display();
-    SudokuSolver slv(&moderateSudoku);
+    testSudoku.display();
+    SudokuSolver slv(&testSudoku);
     std::cout << "After solving:" << std::endl;
     slv.solve();
-    moderateSudoku.display();
+    testSudoku.display();
 
     #endif //DEBUG
 
